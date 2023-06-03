a10165d89ec60d88a56100b8d03e272ed72100a6
chiminhtathoj <vdlong2009@gmail.com>
2023-06-03 22:37:23 +0700

    feature comment

--- client/pages/posts/[postId].tsx
+++ client/pages/posts/[postId].tsx
@@ -4,8 +4,9 @@ import { ClipLoader } from "react-spinners";
 import usePost from "@/hooks/usePost";
 
 import Header from "@/components/Header";
-import Form from "@/components/Forms";
+import Form from "@/components/Form";
 import PostItem from "@/components/posts/PostItem";
+import CommentFeed from "./commentFeed";
 
 
 const PostView = () => {
@@ -27,6 +28,7 @@ const PostView = () => {
       <Header showBackArrow label="Tweet" />
       <PostItem data={fetchedPost} />
       <Form postId={postId as string} isComment placeholder="Tweet your reply" />
+      <CommentFeed comments={fetchedPost?.comments} />
     </>
   );
 }
