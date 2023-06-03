8684c8f861026968fd624df69908b3048ecfa487
chiminhtathoj <vdlong2009@gmail.com>
2023-06-03 22:47:05 +0700

    feature notifications

--- client/components/layout/Sidebar.tsx
+++ client/components/layout/Sidebar.tsx
@@ -1,61 +1,59 @@
-import {BsHouseFill, BsBellFill} from 'react-icons/bs'
-import {FaUser} from 'react-icons/fa'
-import {BiLogOut} from 'react-icons/bi'
-import SidebarLogo from './SidebarLogo'
-import SidebarItem from './SidebarItem'
-import SidebarTweetButton from './SidebarTweetButton'
-import useCurrentUser from '@/hooks/useCurrentUser'
-import { signOut } from 'next-auth/react'
+import { signOut } from 'next-auth/react';
+import { BiLogOut } from 'react-icons/bi';
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+import SidebarItem from './SidebarItem';
+import SidebarLogo from './SidebarLogo';
+import SidebarTweetButton from './SidebarTweetButton';
 
 const Sidebar = () => {
-  const {data: currentUser} = useCurrentUser()
+  const { data: currentUser } = useCurrentUser();
+
   const items = [
     {
+      icon: BsHouseFill,
       label: 'Home',
       href: '/',
-      icon: BsHouseFill
     },
     {
-      label: 'Notifications',
-      href: '/Notifications',
       icon: BsBellFill,
-      auth: true
+      label: 'Notifications',
+      href: '/notifications',
+      auth: true,
+      alert: currentUser?.hasNotification
     },
     {
       icon: FaUser,
       label: 'Profile',
       href: `/users/${currentUser?.id}`,
       auth: true,
-    }
+    },
   ]
 
   return (
-    <div className='col-span-1 h-full pr-4 md:pr-6'>
-      <div className='flex flex-col items-end'>
-        <div className="space-y-2 lg:w-[230px]">
-          <SidebarLogo />
-          {
-            items.map((item) => (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+        <div className="flex flex-col items-end">
+          <div className="space-y-2 lg:w-[230px]">
+            <SidebarLogo />
+            {items.map((item) => (
               <SidebarItem
                 key={item.href}
-                href={item.href}
-                label={item.label}
-                icon={item.icon}
+                alert={item.alert}
                 auth={item.auth}
+                href={item.href}
+                icon={item.icon}
+                label={item.label}
               />
-            ))
-          }
-
-          {
-            currentUser && (
-              <SidebarItem onClick={() => signOut()} icon={BiLogOut} label='Logout' />
-            )
-          }
-          <SidebarTweetButton/>
+            ))}
+            {currentUser && <SidebarItem onClick={() => signOut()} icon={BiLogOut} label="Logout" />}
+            <SidebarTweetButton />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+  )
+};
 
 export default Sidebar;
--- client/pages/api/follow.ts
+++ client/pages/api/follow.ts
@@ -1,31 +1,73 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import prisma from '@/libs/prismadb';
 import serverAuth from "@/libs/serverAuth";
-import prisma from "@/libs/prismadb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
+  if (req.method !== 'POST' && req.method !== 'DELETE') {
     return res.status(405).end();
   }
 
   try {
-    const { currentUser } = await serverAuth(req, res);
-    const { body } = req.body;
-    const { postId } = req.query;
+    const { userId } = req.body;
 
-    if (!postId || typeof postId !== 'string') {
+    const { currentUser } = await serverAuth(req, res);
+
+    if (!userId || typeof userId !== 'string') {
       throw new Error('Invalid ID');
     }
 
-    const comment = await prisma.comment.create({
-      data: {
-        body,
-        userId: currentUser.id,
-        postId
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId
       }
     });
 
-    return res.status(200).json(comment);
+    if (!user) {
+      throw new Error('Invalid ID');
+    }
+
+    let updatedFollowingIds = [...(user.followingIds || [])];
+
+    if (req.method === 'POST') {
+      updatedFollowingIds.push(userId);
+
+      // NOTIFICATION
+      try {
+        await prisma.notification.create({
+          data: {
+            body: 'Someone followed you!',
+            userId,
+          },
+        });
+
+        await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: {
+            hasNotification: true,
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (req.method === 'DELETE') {
+      updatedFollowingIds = updatedFollowingIds.filter((followingId) => followingId !== userId);
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id
+      },
+      data: {
+        followingIds: updatedFollowingIds
+      }
+    });
+
+    return res.status(200).json(updatedUser);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
