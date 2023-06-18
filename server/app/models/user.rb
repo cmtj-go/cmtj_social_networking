e0754fad0f25e4ee84ea8d6ccea2250add411730
chiminhtathoj <vdlong2009@gmail.com>
2023-06-18 20:01:06 +0700

    refator sucture project

--- server/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-	extend Devise::Models
-	devise :database_authenticatable, :registerable,
-					:recoverable, :rememberable, :trackable, :validatable,
-					:omniauthable
-	include DeviseTokenAuth::Concerns::User
-end
